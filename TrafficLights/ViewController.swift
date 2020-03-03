fa1d78accbef80373c4efa7ac4cc97a574da10c5
Eugene St <Eugene.stepchenko@gmail.com>
2020-03-03 07:13:19 +0200

    Added 3 views, added button, created logic to switch alpha property for traffic lights one by one

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -10,11 +10,61 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet var redLIghtView: UIView!
+  @IBOutlet var orangeLightView: UIView!
+  @IBOutlet var greenLightView: UIView!
+  
+  @IBOutlet var startButton: UIButton!
+  
+  var tag = 1
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    initialSettings()
   }
 
+  /// - Summary: create circles if ciew width and height are equal
+  private func circleCorners(for view: UIView) {
+    
+    if view.frame.size.width == view.frame.size.height {
+      view.layer.cornerRadius = view.frame.width/2
+    }
+  }
+  
+  private func initialSettings() {
+    
+    // Rounded corners for Start button
+    startButton.layer.cornerRadius = 20
+    
+    // Calling method to create circled views
+    circleCorners(for: redLIghtView)
+    circleCorners(for: orangeLightView)
+    circleCorners(for: greenLightView)
+    
+    // Settings 0.3 alpha for views
+    redLIghtView.alpha = 0.3
+    orangeLightView.alpha = 0.3
+    greenLightView.alpha = 0.3
+    
+    
+  }
+  
+  @IBAction func buttonPressed() {
+    startButton.setTitle("Next", for: .normal)
+    
+    switch tag {
+    case 1: view.viewWithTag(tag + 2)?.alpha = 0.3; setAlphaAndIncreaseTag()
+    case 2: view.viewWithTag(tag - 1)?.alpha = 0.3; setAlphaAndIncreaseTag()
+    case 3: view.viewWithTag(tag - 1)?.alpha = 0.3; view.viewWithTag(tag)?.alpha = 1.0; tag = 1
+    default: break
+    }
+    
+  }
+  
+  private func setAlphaAndIncreaseTag() {
+    view.viewWithTag(tag)?.alpha = 1.0
+    tag += 1
+  }
 
 }
 
